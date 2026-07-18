29b6e55e104607a0da98d5ae78c79ad667ee7fcb
Ahmed Combo Rachad <161521354+Rachad-ac@users.noreply.github.com>
2026-07-18 00:59:43 +0000

    Change app title from 'Todo List' to 'Mes Taches'

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Todo List',
+      title: 'Mes Taches',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
